8ad3f492acf8da8d0ccc49bbfc9444032b116d4b
Kamal Nayan <sknasylum8454@gmail.com>
2024-03-08 18:09:37 +0530

    db created

--- app.py
+++ app.py
@@ -1,13 +1,26 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']=
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todo.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    desc = db.Column(db.String(500), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self) -> str:
+        return f"{self.sno}-{self.title}"
 
 @app.route("/")
 def hello_world():
+    todo =Todo(title=)
     return render_template("index.html")
 
 @app.route("/contact")
@@ -16,3 +29,7 @@ def contact():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+if __name__ == "__main__":
+    # Create the database file
+    db.create_all()
